63af25112542a0f401346d59b5cc71d5bd31c0b9
onli <onli89@googlemail.com>
2013-11-19 18:56:07 +0100

    fix cancel button

--- designs/default/public/js/main.js
+++ designs/default/public/js/main.js
@@ -36,16 +36,17 @@ snack.ready(function() {
                         // is it an entry?
                         form.querySelector('.editorSubmitButtons').parentNode.insertBefore(cancelButton, form.querySelector('button').parentNode);
                     } catch (e) {
-                        try {
-                            // is it an option?
-                            form.querySelector('input[type="text"]').addEventListener("blur", function(evt) {
+                        var possibleOption = form.querySelector('input[type="text"]');
+                        if (possibleOption.className == "commentFormTel") {
+                            // it is a comment!
+                            form.querySelector('.commentFormSubmit').parentNode.insertBefore(cancelButton, form.querySelector('.commentFormSubmit'));
+                        } else {
+                            // now we know it is not the honeypot of the commentform, so it must be an option
+                            possibleOption.addEventListener("blur", function(evt) {
                                 if (typeof evt.relatedTarget != null || evt.relatedTarget.type == undefined || evt.relatedTarget.type != "submit") {
                                     form.parentNode.replaceChild(parent, form);
                                 }
                             });
-                        } catch (e) {
-                            // it is a comment!
-                            form.querySelector('.commentFormSubmit').parentNode.insertBefore(cancelButton, form.querySelector('.commentFormSubmit'));
                         }
                     }
                     snack.wrap(cancelButton).attach('click', function(evt) {
