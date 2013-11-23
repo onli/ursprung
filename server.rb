6021926c097b7fab4a72ae73a7dcd99fb1ed7dde
onli <onli89@googlemail.com>
2013-11-23 19:28:07 +0100

    changing options invalidate the cache

--- server.rb
+++ server.rb
@@ -345,7 +345,7 @@ end
 post '/setOption' do
     protected!
     Database.new.setOption(params[:name], params[:value])
-    Database.new.invalidateCache
+    Database.new.invalidateCache(nil)   # options are normally mighty enough to invalidate everything
     loadConfiguration
     origin = session[:origin]
     # when setOption wasn't called first, like with the design, origin is old, so unset it
